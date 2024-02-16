fc6814e4f96f3f05d8a424269ae2d37696ef446b
Igor Gritsenko <ctocopokcemb147@yandex.ru>
2024-02-16 14:20:30 +0300

    add card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,38 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import {HeaderModule} from './components/header/header.module';
+import {ProductsListModule} from './pages/products-list/products-list.module';
+
+// View
+// -----------------------
+// Components / Directives
+// Pipe
+
+// Logic
+// --------
+// Services
 
 @NgModule({
-    declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule],
-    providers: [],
+    declarations: [AppComponent], // const
+    exports: [], // module.exports = {}
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        HeaderModule,
+        ProductsListModule,
+    ], // import {...} from '...'
+    // providers: [],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
+
+// HeaderComponent
+// Declaration: HeaderModule,
+// HeaderModule(exports): [HeaderComponent]
+
+// AppModule(imports): [HeaderModule],
